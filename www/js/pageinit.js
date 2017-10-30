9d96f06dbc62bb0674d4856fc319e21696420137
SekdiSOrkf944SSKixoZAD <chiba@sendai-nct.ac.jp>
2017-10-31 01:25:18 +0900

    ナトリスト開発中ｖ１ 情報ページの画像がデータベースからとってきてない

--- www/js/pageinit.js
+++ www/js/pageinit.js
@@ -2,13 +2,14 @@ document.addEventListener('init', function(event) {
   var page = event.target;
   //情報ページ表示時の初期設定
   if (page.id === 'info-page') {
-    console.log(page.data.test);
+    console.log(page.data.title);
     
     document.getElementById("info-title").innerHTML = page.data.title;
     document.getElementById("info-img").src = page.data.img;
     document.getElementById("info-detail").innerHTML = page.data.detail;
   }
 });
+
 
 function onClickInfo(title,detail,img){
     var options = {};
